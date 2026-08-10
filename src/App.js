fd132a7b861d43bcaff578b6084e435dc8c1456d
allensworthbaah <eugene@patchworkcs.org>
2026-08-09 23:30:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeepscode
+        Allensworth Operating System for the Behavioral Health Workforce
     </div>
   );
 }
